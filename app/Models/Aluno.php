9f59887f2583f2caa3a31283a352680dedb48112
Yasminn <carvalhosyasminn@gmail.com>
2026-09-13 16:08:04 -0300

    feat: adiciona models relacionamentos e enum de roles

--- app/Models/Aluno.php
+++ app/Models/Aluno.php
@@ -2,9 +2,22 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Aluno extends Model
 {
-    //
+    use HasFactory;
+
+    protected $fillable = [
+        'nome',
+        'email',
+        'curso_id',
+    ];
+
+    public function curso(): BelongsTo
+    {
+        return $this->belongsTo(Curso::class);
+    }
 }
--- app/Models/Curso.php
+++ app/Models/Curso.php
@@ -2,9 +2,21 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Curso extends Model
 {
-    //
+    use HasFactory;
+
+    protected $fillable = [
+        'nome',
+        'descricao',
+    ];
+
+    public function alunos(): HasMany
+    {
+        return $this->hasMany(Aluno::class);
+    }
 }
