1f946a7d8a7d0c5a39bdb6020da34edec9cd1571
Yasminn <carvalhosyasminn@gmail.com>
2026-09-13 16:30:20 -0300

    feat: adiciona validacao de alunos com form request

--- app/Http/Requests/AlunoRequest.php
+++ app/Http/Requests/AlunoRequest.php
@@ -2,28 +2,27 @@
 
 namespace App\Http\Requests;
 
-use Illuminate\Contracts\Validation\ValidationRule;
 use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Validation\Rule;
 
 class AlunoRequest extends FormRequest
 {
-    /**
-     * Determine if the user is authorized to make this request.
-     */
     public function authorize(): bool
     {
-        return false;
+        return true;
     }
 
-    /**
-     * Get the validation rules that apply to the request.
-     *
-     * @return array<string, ValidationRule|array<mixed>|string>
-     */
     public function rules(): array
     {
         return [
-            //
+            'nome' => ['required', 'string', 'max:255'],
+            'email' => [
+                'required',
+                'email',
+                'max:255',
+                Rule::unique('alunos', 'email')->ignore($this->route('aluno')),
+            ],
+            'curso_id' => ['required', 'exists:cursos,id'],
         ];
     }
 }
