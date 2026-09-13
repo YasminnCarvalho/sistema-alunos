9daf314d5a9812aee073075120d64534dea96a37
Yasminn <carvalhosyasminn@gmail.com>
2026-09-13 16:22:09 -0300

    feat: adiciona middleware de administrador

--- app/Http/Middleware/AdminMiddleware.php
+++ app/Http/Middleware/AdminMiddleware.php
@@ -5,16 +5,20 @@ namespace App\Http\Middleware;
 use Closure;
 use Illuminate\Http\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Illuminate\Support\Facades\Auth;
 
 class AdminMiddleware
 {
-    /**
-     * Handle an incoming request.
-     *
-     * @param  Closure(Request): (Response)  $next
-     */
     public function handle(Request $request, Closure $next): Response
     {
+        if (!Auth::check()) {
+            abort(403);
+        }
+
+        if (Auth::user()->role !== 'admin') {
+            abort(403);
+        }
+
         return $next($request);
     }
 }
